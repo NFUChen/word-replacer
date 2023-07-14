337b3c2469b75cb1b2cdea7e88bba3e6bc2a00b3
William Chen <wichen@sram.com>
2023-07-14 23:17:37 +0800

    add replacements for illegal words in encoded results

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -1,37 +1,37 @@
-
-{
-    "換膚": ["煥膚", "喚膚"], 
-    "消脂": ["輕脂", "告別囤積"]
-}
 import uuid
 from typing import Literal, Any
 from dataclasses import dataclass
+from database_manager import db_manager
 
 @dataclass
 class Typography:
     type: Literal["regular", "warning"]
     content: str
+    replacements: list[str]
 
     def to_dict(self) -> dict[str, str]:
-        return self.__dict__
+        if self.__dict__["type"] == "regular":
+            self.__dict__.pop("replacements")
 
-sentences = [
-    "最新科技讓人類實現換膚的夢想，讓人可以在短短幾小時內改變外貌。",
-    "換膚手術的崛起帶來了許多個人身份的困惑，因為透過換膚, 現在人們可以輕鬆地改變自己的外貌。",
-    "換膚技術的突破不僅在美容界引起轟動，也為了影視和劇院製作提供了更大的自由度，讓演員可以迅速轉換角色形象。"
-]
+
+        return self.__dict__
 
 class Encoder:
     def __init__(self, raw_string: str, illegal_words: list[str]) -> None:
         self.raw_string = raw_string
         self.illegal_words = illegal_words
+        self.suggestions = {
+            word_dict["illegal_word"]: word_dict["suggestions"]
+            for word_dict in db_manager.get_all_words_with_suggesions() 
+        }
+        
 
     def encode(self) -> dict[str, Any]:
         temp_encoded_word = self.raw_string
         id_lookup = {}
         for substring in self.illegal_words:
-            comma_separated = temp_encoded_word.replace(substring, f"||{substring}||")
-            word_list = comma_separated.split("||", -1)
+            sperated_words = temp_encoded_word.replace(substring, f"||{substring}||")
+            word_list = sperated_words.split("||", -1)
             for idx,word in enumerate(word_list):
                 if word == substring:
                     unique_id = str(uuid.uuid4())[:8]
@@ -44,8 +44,8 @@ class Encoder:
                 continue
             _type = "warning" if word in id_lookup else "regular"
             actual_word = id_lookup[word]["word_replaced"] if _type == "warning" else word
-            
-            typographies.append(Typography(_type,actual_word).to_dict())
+            suggestions = self.suggestions.get(actual_word, [])
+            typographies.append(Typography(_type,actual_word, suggestions).to_dict())
         
         result = {
             "source": self.raw_string,
@@ -57,15 +57,13 @@ import json
 
 if __name__ == "__main__":
     sentences = [
-        # "透過換膚，我們可以改變外貌，換膚不僅是改變外貌的方式，更是一種自我轉變的過程。",
-        # "換膚是一個可以改變外貌的選擇，透過換膚，我們可以重新塑造自己的外貌形象。 透過換膚，我們可以改變外貌，換膚不僅是改變外貌的方式，更是一種自我轉變的過程。",
         "換膚是一種改變外貌的方式，透過換膚，我們能夠改變外貌，同時也為自己帶來新的生活體驗。 換膚是一個可以改變外貌的選擇，透過換膚，我們可以重新塑造自己的外貌形象。 透過換膚，我們可以改變外貌，換膚不僅是改變外貌的方式，更是一種自我轉變的過程。"
-
     ]
     substrings = ["換膚", "外貌"]
     for sentence in sentences:
         print(json.dumps(
             Encoder(sentence, substrings).encode(), indent= 4, ensure_ascii=False
         ))
+    # print(Encoder(sentence, substrings).suggestions)
 
 
