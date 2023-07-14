48c6ccccb0aa8e709e108b4c209615206ecfeb07
William Chen <wichen@sram.com>
2023-07-15 01:10:56 +0800

    fix typo in history

--- encoder/history.py
+++ encoder/history.py
@@ -1,28 +1,28 @@
 from typing import Any
 class History:
     def __init__(self) -> None:
-        self.undo = []
-        self.redo = []
+        self._undo = []
+        self._redo = []
     
     def add_action(self, action: Any) -> None:
-        self.undo.append(action)
-        self.redo = []
+        self._undo.append(action)
+        self._redo = []
 
     def undo(self) -> Any:
-        if len(self.undo) == 0:
+        if len(self._undo) == 0:
             raise ValueError("No actions to undo")
 
-        action = self.undo.pop()
-        self.redo.append(action)
+        action = self._undo.pop()
+        self._redo.append(action)
 
         return action
 
     def redo(self) -> Any:
-        if len(self.redo) == 0:
+        if len(self._redo) == 0:
             raise ValueError("No actions to redo")
 
-        action = self.redo.pop()
-        self.undo.append(action)
+        action = self._redo.pop()
+        self._undo.append(action)
 
         return action
 
