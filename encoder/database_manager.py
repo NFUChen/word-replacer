1318800300938835805d53f4addaf4089a0a0e18
RichLi <lichi0623@gmail.com>
2023-07-27 16:30:59 +0800

    translate error message to chinese display

--- encoder/database_manager.py
+++ encoder/database_manager.py
@@ -21,10 +21,10 @@ class DataBaseManager:
     def rename_illegal_word(self, illegal_word: str, new_illegal_word: str) -> None:
         existing_word_dict:dict[str, str | list[str]] = self.db.find_one(self._create_query(illegal_word))
         if not existing_word_dict:
-            raise ValueError(f"{illegal_word} is not in database")
+            raise ValueError(f"危險字詞 {illegal_word} 不存在")
         
         if self._is_naming_conflict(new_illegal_word):
-            raise ValueError(f"{new_illegal_word} already exists in database")
+            raise ValueError(f"危險字詞 {new_illegal_word} 已存在")
         
         existing_word_dict["illegal_word"] = new_illegal_word
         self.db.update_one(self._create_query(illegal_word), {"$set": existing_word_dict})
@@ -32,17 +32,18 @@ class DataBaseManager:
     def update_suggestions(self,illegal_word: str, suggestions: list[str]) -> None:
         existing_word_dict:dict[str, str | list[str]] = self.db.find_one(self._create_query(illegal_word))
         if not existing_word_dict:
-            raise ValueError(f"{illegal_word} is not in database")
+            raise ValueError(f"危險字詞 {illegal_word} 不存在")
         
         existing_word_dict["suggestions"] = suggestions
         if self._is_contains_duplicates(suggestions):
-            raise ValueError(f"{suggestions} contains duplicates")
+            suggestion = suggestions[len(suggestions) - 1]
+            raise ValueError(f"建議字詞 {suggestion} 已存在")
 
         self.db.update_one(self._create_query(illegal_word), {"$set": existing_word_dict})
 
     def add_word_with_suggestions(self, illegal_word: str, suggestions: list[str]) -> None:
         if self.db.find_one(self._create_query(illegal_word)):
-            raise ValueError(f"{illegal_word} already exists in database")
+            raise ValueError(f"危險字詞 {illegal_word} 已存在")
         
         new_word = {
             "illegal_word": illegal_word, 
@@ -58,11 +59,11 @@ class DataBaseManager:
     def remove_word(self, illegal_word: str, word: str) -> None:
         existing_word_dict:dict[str, str | list[str]] = self.db.find_one(self._create_query(illegal_word))
         if not existing_word_dict:
-            raise ValueError(f"{illegal_word} is not in database")
+            raise ValueError(f"危險字詞 {illegal_word} 不存在")
          
         suggestions: list[str] = existing_word_dict["suggestions"]
         if word not in suggestions:
-            raise ValueError(f"{word} is not in {illegal_word} suggestions")
+            raise ValueError(f"建議字詞 {word} 不是危險字詞 {illegal_word} 的選項")
         
         suggestions.remove(word)
         updated = {
@@ -77,7 +78,7 @@ class DataBaseManager:
     def get_suggestions(self, illegal_word: str) -> dict[str, str | list[str]]:
         suggestions = self.db.find_one(self._create_query(illegal_word))
         if not suggestions:
-            raise ValueError(f"{illegal_word} is not in database")
+            raise ValueError(f"危險字詞 {illegal_word} 不存在")
         return suggestions
 
     def get_all_words_with_suggesions(self) -> list[dict[str, str | list[str]]]:
@@ -93,7 +94,7 @@ class DataBaseManager:
             illegal_word["illegal_word"] for illegal_word in list(self.db.find())
         ]
         if len(all_words) == 0:
-            raise ValueError("No illegal words")
+            raise ValueError("尚無危險字詞")
 
         return all_words
     
